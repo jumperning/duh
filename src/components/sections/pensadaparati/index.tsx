05ffdb14544edef072233f971bfa374a8f342b26
jumperning <maxy.m.rodriguez@gmail.com>
2023-09-12 07:39:29 -0300

    add page pensada

---
+++ src/components/sections/pensadaparati/index.tsx
@@ -0,0 +1,13 @@
+
+export const Pensada = () => {
+  return (
+   <>
+   <section className="text-center ">
+    <div className=" -skew-y-6">
+   <span className="text-[#F4CA58] text-xs">Una plataforma interactiva </span></div>
+   <h2 className="text-white Aglet uppercase text-3xl">pensada para TÍ</h2>
+   <p className="text-white text-xs">Métricas y <b> proyecciones de resultados</b> para que puedas tomar la mejor decisión para tu campaña</p>
+   </section>
+   </>
+  )
+}

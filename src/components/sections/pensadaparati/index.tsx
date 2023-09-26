fb62212deacef7b2d2da3e0cf6f5ce336a15d389
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-09-26 20:23:46 -0300

    Add 'Pensada' video selector

--- src/components/sections/pensadaparati/index.tsx
+++ src/components/sections/pensadaparati/index.tsx
@@ -1,14 +1,46 @@
-import { useState } from "react";
-import { PensadaItem, PensadaVideoNav } from "./item"
+import { useEffect, useState } from "react";
+import { PensadaItem, PensadaItemNoIMG, PensadaVideoNav } from "./item"
 
 export const Pensada = () => {
 
   const [selectedId, setSelectedId] = useState("info");
+  const [videoDescription, setVideoDescription] = useState("Cargando...");
+  const [videoTitle, setVideoTitle] = useState("Cargando...");
+  const [videoIcon, setVideoIcon] = useState("");
+  const [videoDataOpacity, setVideoDataOpacity] = useState("opacity-100");
+
+  useEffect(() => {
+    setVideoDataOpacity("opacity-0");
+    setTimeout(() => {
+      switch (selectedId) {
+        case "info":
+          setVideoIcon("img/info.svg");
+          setVideoTitle("Información demográfica y perfiles");
+          setVideoDescription("Lorem ipsum dolor sit amet consectetur. Tellus turpis non consequat dictum faucibus ultricies.");
+          break;
+        case "seleccion":
+          setVideoIcon("img/seleccion.svg");
+          setVideoTitle("Posibilidad de seleccionar perfiles");
+          setVideoDescription("Lorem ipsum dolor sit amet consectetur. Tellus turpis non consequat dictum faucibus ultricies.");
+          break;
+        case "reporte":
+          setVideoIcon("img/reporte.svg");
+          setVideoTitle("Reportes con métricas finales");
+          setVideoDescription("Lorem ipsum dolor sit amet consectetur. Tellus turpis non consequat dictum faucibus ultricies.");
+          break;
+        default:
+          setVideoDescription("Cargando...");
+      }
+    }, 210)
+    setTimeout(() => {
+      setVideoDataOpacity("opacity-100");
+    }, 410)
+
+  }, [selectedId])
 
   return (
     <>
-      <section className="flex flex-col justify-around  items-center text-center mt-12">
-
+      <section className="flex flex-col justify-around items-center text-center mt-12">
         <div className="p-4 gap-5">
           <div className=" -skew-y-3">
             <span className="text-[#F4CA58] text-base md:text-3xl Aglet-Sans">Una plataforma interactiva </span></div>
@@ -23,7 +55,6 @@ export const Pensada = () => {
           <PensadaItem img="img/seleccion.svg" description="Lorem ipsum dolor sit amet consectetur. Tellus turpis non consequat dictum faucibus ultricies.">
             Posibilidad de <br />
             seleccionar perfiles
-            y perfiles
           </PensadaItem>
 
           <PensadaItem img="img/reporte.svg" description="Lorem ipsum dolor sit amet consectetur. Tellus turpis non consequat dictum faucibus ultricies.">
@@ -32,20 +63,29 @@ export const Pensada = () => {
           </PensadaItem>
 
         </div>
-
-      </section>
-      <>
-
-        <div className="relative top-0 m-auto left-0 right-0 flex flex-col justify-center items-center mt-12">
-          <div className="absolute flex justify-center items-center gap-16 z-50 -top-8">
-            <PensadaVideoNav id="info" selectedId={selectedId} setSelectedId={setSelectedId} icon="img/info.svg" />
-            <PensadaVideoNav id="seleccion" selectedId={selectedId} setSelectedId={setSelectedId} icon="img/seleccion.svg" />
-            <PensadaVideoNav id="reporte" selectedId={selectedId} setSelectedId={setSelectedId} icon="img/reporte.svg" />
+        <div className="relative top-0 m-auto left-0 right-0 flex flex-col justify-center items-center mt-12 w-full">
+          <div className="absolute md:flex justify-center items-center gap-16 z-50 -top-8 hidden">
+            <PensadaVideoNav id="info" selectedId={selectedId} setSelectedId={setSelectedId}>
+              <img src="img/info.png" width="50px" />
+            </PensadaVideoNav>
+            <PensadaVideoNav id="seleccion" selectedId={selectedId} setSelectedId={setSelectedId}>
+              <img src="img/seleccion.png" width="50px" />
+            </PensadaVideoNav>
+            <PensadaVideoNav id="reporte" selectedId={selectedId} setSelectedId={setSelectedId}>
+              <img src="img/reporte.png" width="50px" />
+            </PensadaVideoNav>
+          </div>
+          <div className="absolute z-20">
+            <PensadaItemNoIMG opacity={videoDataOpacity} img={videoIcon} description={videoDescription}>
+              {videoTitle}
+            </PensadaItemNoIMG>
+            <img src="img/mirarVideo.svg" alt="" className="z-20 mx-auto" />
           </div>
 
-          <img src="img/mirarVideo.svg" alt="" className="absolute z-20" />
           <img src="img/video.png" alt="video" className="w-full md:w-3/4 relative md:rounded-3xl aspect-video" />
         </div>
+      </section>
+      <>
 
       </>
     </>
--- src/components/sections/pensadaparati/item.tsx
+++ src/components/sections/pensadaparati/item.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 export const PensadaItem = (props: any) => {
     return (
         <article className="flex flex-col justify-center items-center mt-8 md:hidden">
@@ -16,13 +14,29 @@ export const PensadaItem = (props: any) => {
     )
 }
 
+export const PensadaItemNoIMG = (props: any) => {
+    return (
+        <article className={`${props.opacity} md:flex flex-col justify-center items-center mt-8 hidden mb-8 duration-200`}>
+            <figure>
+                <img src={props.img} alt="Info" />
+            </figure>
+            <h3 className="text-yellow-400 text-xl Aglet-bold">
+                {props.children}
+            </h3>
+            <p className="text-white font-light max-w-[400px]">
+                {props.description}
+            </p>
+        </article>
+    )
+}
+
 export const PensadaVideoNav = (props: any) => {
 
     return (
-        <button onClick={() => {props.setSelectedId(props.id)}} className="aspect-square bg-white rounded-lg duration-300" style={{
-            
+        <button onClick={() => {props.setSelectedId(props.id)}} className={`${props.selectedId == props.id ? "shadow-[0px_0px_10px_5px_rgba(255,231,0,0.75)]" : ""} aspect-square bg-white rounded-lg duration-300 hover:scale-110 p-2`} style={{
+
         }}>
-            <img src={props.icon} className="" />
+            {props.children}
         </button>
     )
 }

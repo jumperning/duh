e548c9360bd96562cb2771a6968691e3cfc7a33a
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-09-26 21:16:40 -0300

    Add comolohacemos animation

--- src/components/sections/comolohacemos/index.tsx
+++ src/components/sections/comolohacemos/index.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useRef, useState } from "react";
+import HowItem from "./items"
 
 export const Comolohacemos = () => {
+
+    const [actualScroll, setActualScroll] = useState(0);
+
+    const item1 = useRef<any>(null);
+    const item2 = useRef<any>(null);
+    const item3 = useRef<any>(null);
+    const item4 = useRef<any>(null);
+    const item5 = useRef<any>(null);
+
+    const refs = [item1, item2, item3, item4, item5];
+
+    useEffect(() => {
+        setTimeout(() => {
+            setActualScroll(0);
+            refs[0].current.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
+        }, 2000)
+    }, [])
+
+    useEffect(() => {
+        setTimeout(() => {
+            var auxActualScroll;
+            if (actualScroll == 4) {
+                auxActualScroll = 0;
+            } else {
+                auxActualScroll = actualScroll + 1;
+            }
+            setActualScroll(auxActualScroll);
+            refs[auxActualScroll].current.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
+        }, 2000)
+    }, [actualScroll])
+
     return (
         <section className="text-center gap-4 mt-28 flex flex-col lg:flex-row justify-center items-center ">
             <div className="flex justify-center items-center flex-col lg:text-right">
@@ -9,43 +42,24 @@ export const Comolohacemos = () => {
             </div>
             <div className="relative">
                 <div className="gradient">
-                    <div className="lg:h-[400px] lg:overflow-x lg:overflow-y-auto lg:snap-y ">
-                        <div className="flex flex-row justify-center lg:justify-start tems-center lg:items-start gap-4 p-8">
-                            <div className="number -skew-y-12 font-bold text-[#edcc6b] text-8xl Aglet-italic-bold">1</div>
-                            <div className="text-left">
-                                <h3 className="text-white Aglet-bold mb-2">SELECCIÓN</h3>
-                                <p className="text-white font-light">Seleccionamos los perfiles <br /> adecuados según <b className="Aglet-bold"> la estrategia de marketing </b> de tu marca.</p>
-                            </div>
-                        </div>
-                        <div className="flex flex-row justify-center lg:justify-start tems-center lg:items-start gap-4 p-8">
-                            <div className="number -skew-y-12 font-bold text-[#edcc6b] text-8xl Aglet-italic-bold">2</div>
-                            <div className="text-left">
-                                <h3 className="text-white Aglet-bold mb-2">CONTRATACIÓN</h3>
-                                <p className="text-white font-light">Contratamos y brifeamos a los <b className="Aglet-bold">creadores de contenido.</b> </p>
-                            </div>
-                        </div>
-
-                        <div className="flex flex-row justify-center lg:justify-start tems-center lg:items-start gap-4 p-8">
-                            <div className="number -skew-y-12 font-bold text-[#edcc6b] text-8xl Aglet-italic-bold">3</div>
-                            <div className="text-left">
-                                <h3 className="text-white Aglet-bold mb-2">PRODUCCIÓN Y APROBACIÓN</h3>
-                                <p className="text-white font-light">La producción de contenido pasa por etapas de aprobación entre Duh! y tu equipo.</p>
-                            </div>
-                        </div>
-                        <div className="flex flex-row justify-center lg:justify-start tems-center lg:items-start gap-4 p-8">
-                            <div className="number -skew-y-12 font-bold text-[#edcc6b] text-8xl Aglet-italic-bold">4</div>
-                            <div className="text-left">
-                                <h3 className="text-white Aglet-bold mb-2">GESTIÓN</h3>
-                                <p className="text-white font-light">Gestionamos, calendarizamos y hacemos <b className="Aglet-bold"> seguimiento de las publicaciones.</b></p>
-                            </div>
-                        </div>
-                        <div className="flex flex-row justify-center lg:justify-start tems-center lg:items-start gap-4 p-8">
-                            <div className="number -skew-y-12 font-bold text-[#edcc6b] text-8xl Aglet-italic-bold">5</div>
-                            <div className="text-left">
-                                <h3 className="text-white Aglet-bold mb-2">ENTREGA</h3>
-                                <p className="text-white font-light">Entregamos métricas y reportes online de resultados.</p>
-                            </div>
-                        </div>
+                    <div className="lg:h-[400px] lg:overflow-x-hidden lg:overflow-y-hidden lg:snap-y">
+                        <span className="w-full h-[140px]" />
+                        <HowItem fowardRef={item1} number="1" title="SELECCIÓN">
+                            Seleccionamos los perfiles <br /> adecuados según <b className="Aglet-bold"> la estrategia de marketing </b> de tu marca.
+                        </HowItem>
+                        <HowItem fowardRef={item2} number="2" title="CONTRATACIÓN">
+                            Contratamos y brifeamos a los <b className="Aglet-bold">creadores de contenido.</b>
+                        </HowItem>
+                        <HowItem fowardRef={item3} number="3" title="PRODUCCIÓN Y APROBACIÓN">
+                            La producción de contenido pasa por etapas de aprobación entre Duh! y tu equipo.
+                        </HowItem>
+                        <HowItem fowardRef={item4} number="4" title="GESTIÓN">
+                            Gestionamos, calendarizamos y hacemos <b className="Aglet-bold"> seguimiento de las publicaciones.</b>
+                        </HowItem>
+                        <HowItem fowardRef={item5} number="5" title="SELECCIÓN">
+                            Entregamos métricas y reportes online de resultados.
+                        </HowItem>
+                        <span className="w-full h-[140px]" />
                     </div>
                 </div>
             </div>

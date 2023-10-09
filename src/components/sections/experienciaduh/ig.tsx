ed224e42fd92887f620bc8fa985340ae807af67f
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-10-09 16:45:11 -0300

    Add experienciaduh data

---
+++ src/components/sections/experienciaduh/ig.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+export default function InstagramProfile(props: any) {
+
+    const [followerString, setFollowerString] = useState("");
+
+
+    useEffect(() => {
+        if (props.influencer.followers < 1000) {
+            setFollowerString(props.influencer.followers);
+        }
+        if (props.influencer.followers > 1000 && props.influencer.followers < 1000000) {
+            setFollowerString(Math.floor(props.influencer.followers / 1000) + "K");
+        }
+        if (props.influencer.followers > 1000000) {
+            var auxMillion = Math.floor(props.influencer.followers / 1000000);
+            var auxPoint = Math.floor((props.influencer.followers - auxMillion) / 100000);
+            setFollowerString(auxMillion + "." + auxPoint + "M");
+        }
+    }, [])
+
+    return (
+        <div className="flex flex-row justify-between gap-3 items-center space-y-2">
+            <div className='flex justify-center items-center gap-3'>
+                <div className="from-instagramYellow via-instagramOrange to-instagramPurple relative rounded-full bg-gradient-to-tr p-1">
+                    <a href="#" className="block rounded-full bg-[#F2F2F2] p-1">
+                        <img className="h-16 w-16 max-w-none rounded-full object-cover" src={props.influencer.img} />
+                    </a>
+                </div>
+                <div className='flex flex-col'>
+                    <a href={"https://instagram.com/" + props.influencer.nombre} target="_blank" className="text-lg text-black Aglet-bold font-bold text-left">{props.influencer.nombre}</a>
+                    <span className='text-[#999999]'>{followerString} followers</span>
+                    <a href={"https://instagram.com/" + props.influencer.nombre} target="_blank" className="text-sm text-black Aglet font-bold hidden lg:block">
+                        <img src="img/instagram.svg" alt="" />
+                    </a>
+                </div>
+            </div>
+            <a href={"https://instagram.com/" + props.influencer.nombre} target="_blank" className="text-sm text-black Aglet font-bold block lg:hidden">
+                <img src="img/instagram.svg" alt="" />
+            </a>
+        </div>
+    )
+}
--- src/components/sections/experienciaduh/index.tsx
+++ src/components/sections/experienciaduh/index.tsx
@@ -1,4 +1,12 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import data from './datos.json'
+import { ExperienciaItem } from './items';
+import { Navigation, Pagination } from 'swiper/modules';
 
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 export const Experiencia = () => {
 
@@ -44,71 +52,24 @@ export const Experiencia = () => {
                     </div>
                 </div>
             </div>
-            <div className="bg-[#F2F2F2] flex flex-col lg:flex-row p-4 gap-8 pb-6 lg:mt-20 lg:justify-center lg:items-center lg:w-[1100px] lg:mx-auto lg:max-w-screen-lg lg:rounded-[3rem]">
-                <div className="indicador flex justify-around lg:hidden">
-                    <div className="w-[45%] h-1 bg-[#6A11D6]"></div>
-                    <div className="w-[45%] h-1 bg-[#D9D9D9]"></div>
-                </div>
-
-
-                <article className='text-left flex flex-col justify-center gap-7 max-w-[95vw] pb-4 lg:w-1/2 lg:p-5 lg:gap-5'>
-                    <h3 className='text-[#6A11D6] text-2xl lg:text-3xl Aglet-bold'>Experiencia Duh! Supervielle</h3>
-                    <p className="lg:text-lg">Banco Supervielle presentó su nueva Cuenta Hit y nos eligió para contarlo a través de influencers. </p>
-                    <p className="lg:text-lg">Como parte de la estrategia acompañamos a los creadores de contenido Cristóbal Combes y Juli Castro al impresionante show de Fuerza Bruta, donde el Banco Supervielle instaló su activación para que mostraran en sus redes toda la experiencia.</p>
-                    <h4 className='text-[#6A11D6] text-xl Aglet-bold'>Influencers</h4>
-                    <div className='flex flex-col gap-8'>
-                        <div className="flex flex-row justify-between gap-3 items-center space-y-2">
-                            <div className='flex justify-center items-center gap-3'>
-                                <div className="from-instagramYellow via-instagramOrange to-instagramPurple relative rounded-full bg-gradient-to-tr p-1">
-                                    <a href="#" className="block rounded-full bg-[#F2F2F2] p-1">
-                                        <img className="h-16 w-16 max-w-none rounded-full object-cover" src="img/cristobal.png" />
-                                    </a>
-                                </div>
-                                <div className='flex flex-col'>
-                                    <a href="#" className="text-lg text-black Aglet-bold font-bold text-left">cristobalcombes</a>
-                                    <span className='text-[#999999]'>265k followers</span>
-                                    <a href="#" className="text-sm text-black Aglet font-bold hidden lg:block">
-                                        <img src="img/instagram.svg" alt="" />
-                                    </a>
-                                </div>
-                            </div>
-                            <a href="#" className="text-sm text-black Aglet font-bold block lg:hidden">
-                                <img src="img/instagram.svg" alt="" />
-                            </a>
-                        </div>
-                        <div className="flex flex-row justify-between gap-3 items-center space-y-2">
-                            <div className='flex justify-center items-center gap-3'>
-                                <div className="from-instagramYellow via-instagramOrange to-instagramPurple relative rounded-full bg-gradient-to-tr p-1">
-                                    <a href="#" className="block rounded-full bg-[#F2F2F2] p-1">
-                                        <img className="h-16 w-16 max-w-none rounded-full object-cover" src="img/juli.png" />
-                                    </a>
-                                </div>
-                                <div className='flex flex-col'>
-                                    <a href="#" className="text-lg text-black Aglet-bold font-bold text-left">juli.castroo</a>
-                                    <span className='text-[#999999]'>700k followers</span>
-                                    <a href="#" className="text-sm text-black Aglet font-bold hidden lg:block">
-                                        <img src="img/instagram.svg" alt="" />
-                                    </a>
-                                </div>
-                            </div>
-                            <a href="#" className="text-sm text-black Aglet font-bold block lg:hidden">
-                                <img src="img/instagram.svg" alt="" />
-                            </a>
-                        </div>
-                    </div>
-                    <div className="bg-white rounded-xl p-4 lg:mt-8">
-                        <h3 className='text-[#6A11D6] text-xl Aglet-bold py-2'>Resultados</h3>
-                        <p>
-                            <b>Followers y alcance de audiencia: </b> 370.000 personas <br />
-                            <b>Reproducciones:</b> 600.000 reproducciones<br />
-                            <b> Engagement:</b> 4%
-                        </p>
-                    </div>
-                </article>
-                <div className="w-1/2 hidden sm:block">
-                    <img src="img/superville-ig-story.png" alt="Superville Instagram" />
-                </div>
-            </div>
+            <Swiper
+                // install Swiper modules
+                modules={[Navigation, Pagination]}
+                spaceBetween={10}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+            >
+                {data.map((exp: any, index: number) => {
+                    return (
+                        <SwiperSlide key={index}>
+                            {({ isActive }) => (
+                                <ExperienciaItem isActive={isActive} exp={exp} />
+                            )}
+                        </SwiperSlide>
+                    )
+                })}
+            </Swiper>
         </section>
     )
 }

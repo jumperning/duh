e02cc95e01bf72e4b73af2342270b8d29062cadf
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-10-09 17:08:39 -0300

    Fix slider navigation

--- src/components/sections/experienciaduh/index.tsx
+++ src/components/sections/experienciaduh/index.tsx
@@ -7,8 +7,16 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import { useRef } from 'react';
 
 export const Experiencia = () => {
+
+    const swiperControl: any = useRef<any>();
+
+    function changeTo(index: any) {
+        console.log(swiperControl);
+        swiperControl.current.swiper.slideTo(index);
+    }
 
     return (
         <section className='overflow-hidden text-center mt-24' id="experiencia">
@@ -19,40 +27,41 @@ export const Experiencia = () => {
                 <div className="flex lg:justify-center items-center space-x-6 overflow-x-clip">
                     <div className="flex flex-col lg:justify-center items-center space-y-2">
                         <div className="from-instagramYellow via-instagramOrange to-instagramPurple relative rounded-full bg-gradient-to-tr p-1">
-                            <a href="#" className="block rounded-full bg-[#965BDD] p-1">
+                            <button onClick={() => {changeTo(0)}} className="block rounded-full bg-[#965BDD] p-1">
                                 <img className="h-16 w-16 max-w-none rounded-full object-cover" src="img/Supervielle.png" />
-                            </a>
+                            </button>
                         </div>
-                        <a href="#" className="text-sm text-[#edcc6b] Aglet font-bold">Supervielle</a>
+                        <button onClick={() => {changeTo(0)}} className="text-sm text-[#edcc6b] Aglet font-bold">Supervielle</button>
                     </div>
 
                     <div className="flex flex-col items-center space-y-2">
                         <div className="from-instagramYellow via-instagramOrange to-instagramPurple relative rounded-full bg-gradient-to-tr p-1">
-                            <a href="#" className="block rounded-full bg-[#965BDD] p-1">
+                            <button onClick={() => {changeTo(1)}} className="block rounded-full bg-[#965BDD] p-1">
                                 <img className="h-16 w-16 max-w-none rounded-full object-cover" src="img/Renault.png" />
-                            </a>
+                            </button>
                         </div>
-                        <a href="#" className="text-sm text-white Aglet font-bold">Renault</a>
+                        <button onClick={() => {changeTo(1)}} className="text-sm text-white Aglet font-bold">Renault</button>
                     </div>
                     <div className="flex flex-col items-center space-y-2">
                         <div className="from-instagramYellow via-instagramOrange to-instagramPurple relative rounded-full bg-gradient-to-tr p-1">
-                            <a href="#" className="block rounded-full bg-[#965BDD] p-1">
+                            <button onClick={() => {changeTo(2)}} className="block rounded-full bg-[#965BDD] p-1">
                                 <img className="h-16 w-16 max-w-none rounded-full object-cover" src="img/LATAM.png" />
-                            </a>
+                            </button>
                         </div>
-                        <a href="#" className="text-sm text-white Aglet font-bold">LATAM</a>
+                        <button onClick={() => {changeTo(2)}} className="text-sm text-white Aglet font-bold">LATAM</button>
                     </div>
                     <div className="flex flex-col items-center space-y-2">
                         <div className="from-instagramYellow via-instagramOrange to-instagramPurple relative rounded-full bg-gradient-to-tr p-1">
-                            <a href="#" className="block rounded-full bg-[#965BDD] p-1">
+                            <button onClick={() => {changeTo(3)}} className="block rounded-full bg-[#965BDD] p-1">
                                 <img className="h-16 w-16 max-w-none rounded-full object-cover" src="img/Colgate.png" />
-                            </a>
+                            </button>
                         </div>
-                        <a href="#" className="text-sm text-white Aglet font-bold">Colgate</a>
+                        <button onClick={() => {changeTo(3)}} className="text-sm text-white Aglet font-bold">Colgate</button>
                     </div>
                 </div>
             </div>
             <Swiper
+                ref={swiperControl}
                 // install Swiper modules
                 modules={[Navigation, Pagination]}
                 spaceBetween={10}
